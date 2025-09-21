d458958fb1a75a1c0f7233383d6e7b5935f045be
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2025-09-21 21:45:00 +0100

    Fixed syntax issues in plantacao

--- src/main/java/hortas_grupo2/Plantacao.java
+++ src/main/java/hortas_grupo2/Plantacao.java
@@ -3,29 +3,19 @@ package hortas_grupo2;
 import java.time.LocalDate;
 
 public class Plantacao {
+  private LocalDate dataSementeira;
+  private String tipoCultura;
 
-	
-	public class Plantacao {
-	    private LocalDate dataSementeira;
-	    private String tipoCultura;
-	    
-	    
-	    public Plantacao(LocalDate dataSementeira, String tipoCultura) {
-	        this.dataSementeira = dataSementeira;
-	        this.tipoCultura = tipoCultura;
-	    }
+  public Plantacao(LocalDate dataSementeira, String tipoCultura) {
+    this.dataSementeira = dataSementeira;
+    this.tipoCultura = tipoCultura;
+  }
 
-	   
-	    public String toString() {
-	        return "[Data de sementeira: " + dataSementeira + ", Tipo de cultura: " + tipoCultura + "]";
-	    }
+  public String toString() {
+    return "[Data de sementeira: " + dataSementeira + ", Tipo de cultura: " + tipoCultura + "]";
+  }
 
-	    
-	    public String getTipoCultura() {
-	        return tipoCultura;
-	    }
-	}
-	
-
-  
+  public String getTipoCultura() {
+    return tipoCultura;
+  }
 }
