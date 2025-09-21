9f2c74a46b49b9333c5d66942cd8966a7db2b64b
ricar <ricar@Ricky.Home>
2025-09-21 20:08:32 +0100

    editei a class platacao

--- src/main/java/hortas_grupo2/Plantacao.java
+++ src/main/java/hortas_grupo2/Plantacao.java
@@ -1,6 +1,31 @@
 package hortas_grupo2;
 
+import java.time.LocalDate;
+
 public class Plantacao {
+
+	
+	public class Plantacao {
+	    private LocalDate dataSementeira;
+	    private String tipoCultura;
+	    
+	    
+	    public Plantacao(LocalDate dataSementeira, String tipoCultura) {
+	        this.dataSementeira = dataSementeira;
+	        this.tipoCultura = tipoCultura;
+	    }
+
+	   
+	    public String toString() {
+	        return "[Data de sementeira: " + dataSementeira + ", Tipo de cultura: " + tipoCultura + "]";
+	    }
+
+	    
+	    public String getTipoCultura() {
+	        return tipoCultura;
+	    }
+	}
+	
 
   
 }
